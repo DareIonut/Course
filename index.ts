1db07e529c476faa8dfa4454c84bfa0bad1f699d
DareIonut <daniel.dare@nc-vision.com>
2023-07-14 16:37:20 +0300

    Section 1 DONE

--- index.ts
+++ index.ts
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo{
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    const todo = response.data;
 
-    const ID = todo.ID;
-    const title = todo.Title;
-    const finished = todo.finished;
 
-    console.log(`There is the ${ID}, the ${title} and ${finished}`)
+    const todo = response.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+   logTodo(id, title, completed)
 });
 
+const logTodo = (id: number, title: string, completed: boolean)=>{
+    console.log(`There is the ${id}, the ${title} and ${completed}`)
+}
