d06ed4a1fbd399dee24ae982e8888f15f4b5985f
DareIonut <daniel.dare@nc-vision.com>
2023-07-21 09:02:05 +0300

    Section 3 - Done

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,5 +1,4 @@
 //Primitive types
-
 let helloName: string = 'Hello there is a string';
 let speed: number = 4;
 let hasName: boolean = true;
@@ -8,23 +7,45 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 //Object Types
-
 let colors: string[] = ['red', 'green', 'blue'];
 let speedTicked: number[] = [0.12, 44.5, 25.4];
 let sayTruth: boolean[] = [true, false, true];
-
 class Car {}
-
 let vw: Car = new Car();
 
 //Object literal
-
 const point: { x: number; y: number } = { x: 20, y: 30 };
 
 //Function annotations
-
 const sum: (x: number, y: number) => number = (x: number, y: number) => {
   return x + y;
 };
 
-sum(1, 3);
+//When to use annotations
+
+// 1. Function that returns the 'any'type
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2. When we declare a variable on one line line and initialize it later
+
+let words = ['red', 'blue', 'green'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3. Variable whose type cannot be inferred correctly
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
